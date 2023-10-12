74152b9196dc69890182c2529b8fa4819e2b0074
Baocheng Wang <baocheng0121@gmail.com>
2023-10-13 00:45:03 +1100

    <raw> can add card now

--- app/src/main/java/com/example/soga/RoomManagement.java
+++ app/src/main/java/com/example/soga/RoomManagement.java
@@ -1,12 +1,17 @@
 package com.example.soga;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class RoomManagement extends AppCompatActivity {
 
@@ -23,7 +28,30 @@ public class RoomManagement extends AppCompatActivity {
         EditText roomName = findViewById(R.id.room_name);
         EditText roomCapacity = findViewById(R.id.room_capacity);
 
+// 创建新的CardView
+        CardView newCard = new CardView(getApplicationContext());
+        newCard.setLayoutParams(new CardView.LayoutParams(
+                CardView.LayoutParams.MATCH_PARENT,
+                CardView.LayoutParams.WRAP_CONTENT
+        ));
 
+        newCard.setCardElevation(4);
+
+//        newCard.setCardCornerRadius(8);
+        newCard.setCardBackgroundColor(Color.WHITE);
+        TextView textView = new TextView(getApplicationContext());
+        textView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        textView.setText("新的卡片视图");
+        textView.setPadding(16, 16, 16, 16);
+
+        // 将TextView添加到CardView中
+        newCard.addView(textView);
+
+        // 将新的CardView添加到容器中
+        cardContainer.addView(newCard);
 
         Toast.makeText(this, "HAHAHAHAH", Toast.LENGTH_SHORT).show();
 
