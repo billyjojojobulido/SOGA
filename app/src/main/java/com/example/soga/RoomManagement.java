7cf43ca4ab7cf1f69f1b7cf6549f1626ba15b9fe
Baocheng Wang <baocheng0121@gmail.com>
2023-10-13 00:33:34 +1100

    add endpoint :: task dropdown

--- app/src/main/java/com/example/soga/RoomManagement.java
+++ app/src/main/java/com/example/soga/RoomManagement.java
@@ -3,6 +3,10 @@ package com.example.soga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class RoomManagement extends AppCompatActivity {
 
@@ -11,4 +15,18 @@ public class RoomManagement extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_management);
     }
+
+    public void createEndpoint(View view){
+        LinearLayout cardContainer = findViewById(R.id.card_layout);
+
+
+        EditText roomName = findViewById(R.id.room_name);
+        EditText roomCapacity = findViewById(R.id.room_capacity);
+
+
+
+        Toast.makeText(this, "HAHAHAHAH", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
