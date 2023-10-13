14219454afb17e296e0d0e9baf306a55e1a7a970
Baocheng Wang <baocheng0121@gmail.com>
2023-10-13 16:18:45 +1100

    <wip> check location HTTP request

--- app/src/main/java/com/example/soga/RoomManagement.java
+++ app/src/main/java/com/example/soga/RoomManagement.java
@@ -14,12 +14,61 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class RoomManagement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_management);
+    }
+
+    public void checkLocation(View view){
+
+        EditText addressInput = findViewById(R.id.location_input);
+        String address = addressInput.getText().toString();
+
+        if (address.length() == 0) {
+            Toast.makeText(this, "Input an address first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        try {
+            String query = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+                    address
+                    + "&key=AIzaSyB_4H-LgBByty7rCuZR4DSagLK6c7y1rYY";
+
+            URL url = new URL(query);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            // timeout checking
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            // Retrieve Response Status Code
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = connection.getInputStream();
+
+                
+
+                // TODO
+                // 注意：你需要在主线程之外处理UI操作
+            } else {
+                // Handle Error
+                Toast.makeText(this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+
+            connection.disconnect();
+        } catch (IOException e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void createEndpoint(View view){
@@ -35,6 +84,7 @@ public class RoomManagement extends AppCompatActivity {
                 CardView.LayoutParams.MATCH_PARENT,
                 CardView.LayoutParams.WRAP_CONTENT
         ));
+
 
         newCard.setCardElevation(4);
 
@@ -53,8 +103,6 @@ public class RoomManagement extends AppCompatActivity {
 
         // Add new CardView to Container
         cardContainer.addView(newCard);
-
-//        Toast.makeText(this, "HAHAHAHAH", Toast.LENGTH_SHORT).show();
 
     }
 
