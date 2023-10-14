5181c0e23ba1164b260d95299dec1ceae56a80de
Yanhao Xu <yaxu5503@uni.sydney.edu.au>
2023-10-14 16:47:03 +1100

    jump detection add progress bar

--- app/src/main/java/com/example/soga/JumpActivity.java
+++ app/src/main/java/com/example/soga/JumpActivity.java
@@ -1,6 +1,7 @@
 package com.example.soga;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,7 +9,11 @@ import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +27,11 @@ public class JumpActivity extends AppCompatActivity implements SensorEventListen
     private long lastJumpTime = 0;
     private int jumpCount = 0;
     TextView myEditText;
+    private ProgressBar progressBar;
+    private Button start;
+    private int realValue = 0;
+    private boolean flag = false;
+    Button finish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +42,12 @@ public class JumpActivity extends AppCompatActivity implements SensorEventListen
         if (sensorManager != null) {
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         }
-//        onSensorChanged()
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+        finish = findViewById(R.id.finish);
+
+
     }
 
     @Override
@@ -55,44 +70,80 @@ public class JumpActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float[] values = event.values;
+        if (flag == true) {
+            float[] values = event.values;
 
-        // Detect upward acceleration
-        float acceleration = (float) Math.sqrt(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]);
-        long currentTime = 0;
-        if (acceleration > 10) {
-            currentTime = System.currentTimeMillis();
+            // Detect upward acceleration
+            float acceleration = (float) Math.sqrt(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]);
+            long currentTime = 0;
+            if (acceleration > 11) {
+                currentTime = System.currentTimeMillis();
 
-            // Check if enough time has passed since the last jump
-            if (currentTime - lastJumpTime > JUMP_DETECTION_INTERVAL) {
-                // Count this as a jump
-                jumpCount++;
-                lastJumpTime = currentTime;
+                // Check if enough time has passed since the last jump
+                if (currentTime - lastJumpTime > JUMP_DETECTION_INTERVAL) {
+                    // Count this as a jump
+                    jumpCount++;
+                    lastJumpTime = currentTime;
 
-                // Do something when a jump is detected (e.g., increment a counter)
+                    // Do something when a jump is detected (e.g., increment a counter)
 
+                }
             }
-        }
-        Log.d("mytag",Integer.toString(jumpCount));
-        if(jumpCount > 5){
+            if(jumpCount >= 10) {
+                jumpCount = 10;
+            }
+            Log.d("mytag",Integer.toString(jumpCount));
+
+            progressBar.setProgress(10*jumpCount);
+//        if(jumpCount > 5){
             myEditText = findViewById(R.id.acceValue);
             // Now you can work with myEditText
 
-            myEditText.setText("True");
+            myEditText.setText(Integer.toString(jumpCount) + "/10");
+//        }
+        }
+        if(jumpCount >= 10) {
+//            flag = false;
+            finish.setVisibility(View.VISIBLE);
         }
     }
 
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-
+        return;
     }
 
-    private boolean isJumping(float[] accelerometerValues) {
-        // Implement your logic to detect jumping based on accelerometer readings
-        // This can involve analyzing the values of the accelerometer to recognize a specific motion pattern
-        // For simplicity, let's assume jumping if the Y-axis acceleration is above a certain threshold
-        float yAcceleration = accelerometerValues[1];
-        return yAcceleration > 10.0; // Adjust the threshold as needed
+    // Example method to simulate a time-consuming task
+    private void updateProgressBar(ProgressBar progressBar) {
+        // Reference the ProgressBar
+        // Show the ProgressBar
+        // Update the real value (replace this with your actual logic)
+        realValue = 10;
+//
+//        // Ensure the real value doesn't exceed the maximum progress
+//        if (realValue > progressBar.getMax()) {
+//            realValue = progressBar.getMax();
+//        }
+
+        // Update the ProgressBar based on the real value
+        progressBar.setProgress(realValue*jumpCount/2);
+
+
+    }
+    public void onButtonClick(View view) {
+//        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        flag = true;
+//        updateProgressBar(progressBar);
+    }
+
+    public void onButtonClickFinish(View view) {
+//        progressBar = findViewById(R.id.progressBar);
+        startActivities(new Intent[]{new Intent(this, MapsActivity.class)});
+
+//        updateProgressBar(progressBar);
     }
 
 
