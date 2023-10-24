257fdcef8457cf1c8ca764d475253e2c3a7fc3c7
Baocheng Wang <baocheng0121@gmail.com>
2023-10-24 17:07:27 +1100

    <db> create room bounded with DB

--- app/src/main/java/com/example/soga/MainActivity.java
+++ app/src/main/java/com/example/soga/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         } else {// if not,jump to log in page
             intent = new Intent(this, Login.class);
         }
-        intent = new Intent(this, HoldActivity.class);
+        intent = new Intent(this, RoomManagement.class);
         startActivity(intent);
         finish();
 
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
