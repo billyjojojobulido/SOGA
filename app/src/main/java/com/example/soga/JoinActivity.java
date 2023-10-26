fc13c020b107b1adfb468fa0a6527914bb55c01e
Elena-98 <xue12@student.unimelb.edu.au>
2023-10-26 20:09:40 +1100

    download endpoints from firebase

--- app/src/main/java/com/example/soga/JoinActivity.java
+++ app/src/main/java/com/example/soga/JoinActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,11 +18,17 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JoinActivity extends AppCompatActivity {
 
     private Button confirmBtn;
-    private TextView joinCode;
+    private EditText joinCode;
     private FirebaseFirestore db;
+    private ArrayList<HashMap<String, Object>> endpoints;
+    private TextView textPop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class JoinActivity extends AppCompatActivity {
 
         confirmBtn = findViewById(R.id.confirmBtn);
         joinCode = findViewById(R.id.textJoinCode);
+        textPop = findViewById(R.id.textPop);
 
         db = FirebaseFirestore.getInstance();
 
@@ -38,46 +46,60 @@ public class JoinActivity extends AppCompatActivity {
 
 //    the user need to click the button to confirm their join codes
     public void onButtonClickCode(View view){
+        textPop.setText("");
 
-        confirmBtn.setText("Checking");
         String inputCode = joinCode.getText().toString();
 
         if (inputCode.isEmpty()){
-            joinCode.setText("Please input valid code.");
+            textPop.setText("Please input valid code.");
+            joinCode.setText("Code: ");
             return;
+        }else{
+            confirmBtn.setText("Checking");
+            checkJoin(inputCode);
         }
 
-        db.collection("rooms").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        String codeFromDb = document.getString("code");
-                        if (inputCode.equals(codeFromDb)) {
-                            confirmBtn.setText("Joining");
 
-                        }
-                    }
-                } else {
-                    Log.d(TAG, "Error getting documents: ", task.getException());
-                }
-            }
-        });
 
 
     }
 
 //    after entering the code, it will be checked to the firebase to see if the room truly exists
-    private void checkJoin(){
+    private void checkJoin(String inputCode){
+        db.collection("rooms").whereEqualTo("code", inputCode).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult().isEmpty()){
+                        textPop.setText("No such a room");
+                    }else {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+
+                            // Get the "endpoints" field which is a hashmap
+                            endpoints = (ArrayList<HashMap<String, Object>>) document.get("endpoints");
+
+//                            testing purpose
+//                            textPop.setText(endpoints.get(0).get("answer").toString());
+
+                            if (endpoints != null) {
+                                // Do something with the "endpoints" hashmap
+                            }
+                        }
+                    }
+                } else {
+                        Log.d(TAG, "Error retrieving documents: ", task.getException());
+                }
+            }
+        });
+
+
 
     }
 
 //    after checking the join code, if true then download the game details and jump to map page
 //    else jump to the main page or ask for the code again
-    private void forward(){
 
+    public ArrayList<HashMap<String, Object>> getEndpoints() {
+        return endpoints;
     }
-
-
-
 }
