ff0550857e786579805ae7b4cfee8e1591885f19
Baocheng Wang <baocheng0121@gmail.com>
2023-10-31 23:57:03 +1100

    reconstruct Elena's join room func to cooperate with Game construction

--- app/src/main/java/com/example/soga/JoinActivity.java
+++ app/src/main/java/com/example/soga/JoinActivity.java
@@ -12,9 +12,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -31,6 +37,8 @@ public class JoinActivity extends AppCompatActivity {
     private ArrayList<HashMap<String, Object>> endpoints;
     private TextView textPop;
     private Boolean clicked = false;
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +70,7 @@ public class JoinActivity extends AppCompatActivity {
 
             } else {
                 confirmBtn.setText("Checking");
-                checkJoin(inputCode);
+                joinRoom(inputCode);
 
             }
             confirmBtn.setText("Confirm");
@@ -71,43 +79,86 @@ public class JoinActivity extends AppCompatActivity {
 
     }
 
-//    after entering the code, it will be checked to the firebase to see if the room truly exists
-    private void checkJoin(String inputCode){
-        db.collection("rooms").whereEqualTo("code", inputCode).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+    private boolean canJoin(String code){
+        final boolean[] ret = {false};  // element array -> inner element assignment;
+        db.collection("rooms").whereEqualTo("code", code).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    if (task.getResult().isEmpty()){
-                        textPop.setText("Wrong code. Please check it.");
+                if (task.isSuccessful()){
+                    if (task.isSuccessful()) {
+                        if (task.getResult().isEmpty()){
+                            ret[0] = false;
+                        }else {
+                            ret[0] = true;
 
-                    }else {
-
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            confirmBtn.setText("Joining");
-                            // Get the "endpoints" field which is a hashmap
-                            endpoints = (ArrayList<HashMap<String, Object>>) document.get("endpoints");
-
-//                            testing purpose
-//                            textPop.setText(endpoints.get(0).get("answer").toString());
-
-                            if (endpoints == null) {
-                                textPop.setText("The room no longer exists");
-                                joinCode.setText("Code: ");
-
-                                return;
-                            }else{
-//                                should start the game and enter in map page
-                                startActivities(new Intent[]{new Intent(JoinActivity.this, MapsActivity.class)});
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                endpoints = (ArrayList<HashMap<String, Object>>) document.get("endpoints");
+                                if (endpoints == null) {
+                                    textPop.setText("The room no longer exists");
+                                    ret[0] = false;
+                                }
+                                break;
                             }
                         }
                     }
                 } else {
-                        Log.d(TAG, "Error retrieving documents: ", task.getException());
+                    Log.d(TAG, "Error retrieving Rooms documents: ", task.getException());
                 }
             }
         });
 
+        return ret[0];
     }
+
+    private void joinRoom(String code){
+        /** Check if the user is properly logged in
+         * */
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            textPop.setText("Invalid User Id, please log in again.");
+            return;
+        }
+        String uid = user.getUid();
+
+        /** Check if the room exists
+         * */
+        if (!canJoin(code)){
+            textPop.setText("Room does not exist, please try a valid code.");
+            return;
+        }
+
+        /**
+         * Join a New Game
+         * */
+        confirmBtn.setText("Joining");
+        long currentTimeStamp = System.currentTimeMillis() / 1000;
+
+        Map<String, Object> game = new HashMap<>();
+        game.put("uid", uid);
+        game.put("roomCode", code);
+        game.put("progress", 0);
+        game.put("startTime", currentTimeStamp);
+        game.put("steps", 0);
+
+        // Add a new document with a generated ID
+        db.collection("games").add(game).addOnSuccessListener(
+                new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                        startActivities(new Intent[]{new Intent(JoinActivity.this, MapsActivity.class)});
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(JoinActivity.this,"Failed", Toast.LENGTH_SHORT).show();
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+
     public void back(View view){
         finish();
     }
