7a2e1f861a4e4f94c0240846299a1181984ac29b
HAOOO99 <xuyanhao2000@gmail.com>
2023-11-03 17:28:41 +1100

    set the camera location aroung the user

--- app/src/main/java/com/example/soga/MapsActivity.java
+++ app/src/main/java/com/example/soga/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.example.soga;
 
 
+import static androidx.fragment.app.FragmentManager.TAG;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
@@ -23,10 +25,17 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -37,6 +46,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.soga.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 import android.Manifest;
@@ -69,6 +80,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private SensorManager sensorManager;
     private Sensor sensor;
     private Marker userMarker;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
 
 
     private final ActivityResultLauncher<Intent> activityResultLauncher =
@@ -131,6 +144,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         TextView progressView = findViewById(R.id.progress_text);
         progressView.setText("Progress: " + progress + " / " + totalProgress);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(500);
+        locationRequest.setFastestInterval(500);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         // check location permission
 //        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -145,63 +164,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        }
 
 
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        SensorEventListener sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-//                float azimuth = event.values[0];
-//                // 此处获取到的azimuth值表示设备的方向
-//                // 使用azimuth来更新地图的方向
-//                updateMapDirection(azimuth);
-                if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-                    float[] rotationMatrix = new float[9];
-                    float[] orientationValues = new float[3];
-
-                    SensorManager.getRotationMatrix(rotationMatrix, null, null, event.values);
-                    SensorManager.getOrientation(rotationMatrix, orientationValues);
-
-                    float azimuthInRadians = orientationValues[0];
-                    float azimuthInDegrees = (float) Math.toDegrees(azimuthInRadians);
-
-                    // Update marker rotation
-//                    if (yourMarker != null) {
-//                        yourMarker.setRotation(azimuthInDegrees);
-//                    }
-                    System.out.println(azimuthInDegrees);
-
-                    updateMapDirection(azimuthInDegrees);
-                }
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-                // 精度变化时的处理，可以忽略
-            }
-        };
-
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
     }
 
-    private void updateMapDirection(float azimuth) {
-        if (mMap != null && userMarker != null) {
-            // 使用CameraPosition设置地图方向
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(currentLatLng) // 当前位置
-                    .zoom(mMap.getCameraPosition().zoom) // 缩放级别
-                    .bearing(azimuth) // 设置地图方向
-                    .build();
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            userMarker.setRotation(azimuth);
-        }
-    }
+
 
     /**
      * Manipulates the map once available.
@@ -222,6 +192,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
+            Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+            locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    System.out.println("on success onLocationResult: " + location.getLatitude());
+
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    float zoomLevel = 20.0f; // Adjust the zoom level as needed
+
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(latLng)           // Sets the center of the map to the new location
+                            .zoom(zoomLevel)          // Sets the zoom
+                            // .bearing(azimuth)      // Uncomment and set bearing if needed
+                            // .tilt(tiltAngle)       // Uncomment and set tilt if needed
+                            .build();
+
+                    // Animate the camera to the new
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+            });
+//            zoomToUserLocation();
 //            getCurrentLocation();
 
 //            zoomToUserLocation();
@@ -237,24 +232,106 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
 //        -37.79970759026579, 144.9636742373955
 //        -37.80364308009827, 144.96373452399772
-        getCurrentLocation();
+//        getCurrentLocation();
 //        LatLng unimelb = new LatLng(-37.80364308009827, 144.96373452399772);
-        LatLng unimelb = currentLatLng;
+//        LatLng unimelb = currentLatLng;
 //        mMap.addMarker(new MarkerOptions().position(unimelb).title("Marker in Unimelb"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(unimelb, 18));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(unimelb, 18));
 
-        MarkerOptions markerOptions = new MarkerOptions()
-                .position(currentLatLng)
-//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.direction))
-                .anchor(0.5f, 0.5f);
-        int width = 50; // 标记宽度（像素）
-        int height = 50; // 标记高度（像素）
-        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.direction);
-        Bitmap b = bitmapdraw.getBitmap();
-        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
-        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));// 设置箭头图标
-        userMarker = mMap.addMarker(markerOptions);
+//        MarkerOptions markerOptions = new MarkerOptions()
+//                .position(currentLatLng)
+////                .icon(BitmapDescriptorFactory.fromResource(R.drawable.direction))
+//                .anchor(0.5f, 0.5f);
+//        int width = 50; // 标记宽度（像素）
+//        int height = 50; // 标记高度（像素）
+//        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.direction);
+//        Bitmap b = bitmapdraw.getBitmap();
+//        Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+//        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker));// 设置箭头图标
+//        userMarker = mMap.addMarker(markerOptions);
 //        userMarker.setRotation(headingAngle);
+
+    }
+
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            double latitude = locationResult.getLastLocation().getLatitude();
+            double longitude = locationResult.getLastLocation().getLongitude();
+
+            // Create a LatLng object
+
+//            System.out.println("onLocationResult: " + locationResult.getLastLocation());
+            currentLatLng = new LatLng(latitude,longitude);
+//            double latitude = locationResult.getLastLocation().getLatitude();
+//            double longitude = locationResult.getLastLocation().getLongitude();
+            float zoomLevel = 20.0f; // Adjust the zoom level as needed
+
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(latLng)           // Sets the center of the map to the new location
+                    .zoom(zoomLevel)          // Sets the zoom
+                    // .bearing(azimuth)      // Uncomment and set bearing if needed
+                    // .tilt(tiltAngle)       // Uncomment and set tilt if needed
+                    .build();
+
+            // Animate the camera to the new
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//            if (mMap != null) {
+//                setUserLocationMarker(locationResult.getLastLocation());
+//            }
+        }
+    };
+
+    private void startLocationUpdates() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationUpdates();
+        } else {
+            // you need to request permissions...
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopLocationUpdates();
+    }
+
+    private void zoomToUserLocation() {
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
 
     }
 
@@ -285,7 +362,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
         currentLatLng = new LatLng(Default_Lat, Default_Lng);
-//        System.out.println(Default_Lat);
+        System.out.println(Default_Lat + Default_Lng);
 //        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18));
     }
 
@@ -346,6 +423,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void arrivalCheck(View view) {
+        System.out.println("arrival check "+currentLatLng.latitude);
         String endpointLat = (String) endpoints.get(progress).get("lat");
         String endpointLon = (String) endpoints.get(progress).get("lng");
         Long taskCode = (Long) endpoints.get(progress).get("task");
@@ -353,7 +431,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         try {
             double lat = Double.parseDouble(endpointLat);
             double lon = Double.parseDouble(endpointLon);
-            boolean isNearBy = isLocationNearby(lat, lon, 5);
+            boolean isNearBy = isLocationNearby(lat, lon, 100);
             if (isNearBy) {
                 System.out.println("You are here.");
                 /**
@@ -393,8 +471,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         float distance = currentLocation.distanceTo(targetLocation);
 
         // Return true if the distance is less than or equal to the threshold, false otherwise
-//        return distance <= distanceThreshold;
-        return true;
+        return distance <= distanceThreshold;
+//        return true;
     }
 
 
@@ -429,6 +507,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             } else {
 //                getCurrentLocation();
                 mMap.setMyLocationEnabled(true);
+                Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+                locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        System.out.println("on success onLocationResult: " + location.getLatitude());
+
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        float zoomLevel = 20.0f; // Adjust the zoom level as needed
+
+                        LatLng latLng = new LatLng(latitude, longitude);
+
+                        CameraPosition cameraPosition = new CameraPosition.Builder()
+                                .target(latLng)           // Sets the center of the map to the new location
+                                .zoom(zoomLevel)          // Sets the zoom
+                                // .bearing(azimuth)      // Uncomment and set bearing if needed
+                                // .tilt(tiltAngle)       // Uncomment and set tilt if needed
+                                .build();
+
+                        // Animate the camera to the new
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    }
+                });
+                zoomToUserLocation();
                 Toast.makeText(this, "Location permission dined.",Toast.LENGTH_SHORT).show();
             }
         }
